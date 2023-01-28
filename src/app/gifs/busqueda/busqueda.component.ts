ef07ec322fbee4b6fb1335d9f6dc405392431312
Alexisgonz <alexisgonzalezf07@gmail.com>
2023-01-27 22:37:03 -0500

    app-heroes

---
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,26 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html'
+})
+export class BusquedaComponent {
+
+  constructor(private gifsService: GifsService){}
+
+  @ViewChild('txtBuscar') txtBuscar!:ElementRef<HTMLInputElement>
+
+  buscar () {
+    const valor = this.txtBuscar.nativeElement.value
+
+    if (valor.trim().length === 0)
+    return;
+
+    this.gifsService.buscarGift(valor)
+
+    this.txtBuscar.nativeElement.value ='';
+    
+  }
+
+}
